2d3fe3bfc29f386eaa85ee7062259e30e7a66d2d
theIanMilan <theIanMilan@users.noreply.github.com>
2021-11-15 19:08:37 +0800

    Configure google credentials

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -22,11 +22,9 @@ module HomeHelper
   end
 
   def create_google_sheets_session_and_get_worksheet
-    # Creates a session. This will prompt the credential via command line for the
-    # first time and save it to config.json file for later usages.
-    # See this document to learn how to create config.json:
-    # https://github.com/gimite/google-drive-ruby/blob/master/doc/authorization.md
-    session = GoogleDrive::Session.from_service_account_key('client_secret.json')
+    # Creates a Google Sheets session using service_account.json obtained from Google API
+    client_secret = ENV.fetch('GOOGLE_APPLICATION_CREDENTIALS')
+    session = GoogleDrive::Session.from_service_account_key(client_secret)
     session.spreadsheet_by_key(ENV.fetch('SPREADSHEET_ID')).worksheets[0]
   end
 end
